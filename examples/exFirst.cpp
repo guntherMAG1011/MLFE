88d37ce6a59a3cf185d7d9ed4ec968e2d29ad976
rohit <rohit_peram@yahoo.com>
2023-01-29 23:10:17 +0530

    init googletest

--- examples/exFirst.cpp
+++ examples/exFirst.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "lafe/structure.hpp"
+#include "GLFW/glfw3.h"
 
 int main() {
 	std::cout << "Hello Sivaram!" << '\n';
--- modules/lafe/matrixOps.cpp
+++ modules/lafe/matrixOps.cpp
@@ -1,3 +1,4 @@
 #include <iostream>
 
 #include "lafe/structure.hpp"
+#include "GLFW/glfw3.h"
