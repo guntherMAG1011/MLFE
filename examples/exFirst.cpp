eb4278652c00be7c9369fa8908502c36af747fc0
Gunther <gunther1011.linux@gmail.com>
2023-02-13 18:09:38 +0530

    cleaned everything

--- examples/exFirst.cpp
+++ examples/exFirst.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <blaze/Blaze.h>
 
-#include "lafe/structure.hpp"
+using namespace blaze;
 
 int main() {
-	int array[4] = { 1, 2, 3, 4 };
-	std::cout << array << '\n';
-	int* ptr = array + 5;
-	std::cout << *ptr << '\n';
-	std::vector<int> v = { 1, 2, 3, 4 };
-	std::cout << v.data() << '\n';
-	LAFECPP::Vector<int> vec1(v);
-	std::cout << "Dimension of the vector: " << vec1.getDimension() << '\n';
-	std::cout << "Magnitude of the vector: " << vec1.getMagnitude() << '\n';
+	setNumThreads(4);
+    const size_t N(100UL);
+    const size_t iterations(10UL);
+
+    const size_t NN(N * N);
+
+    blaze::CompressedMatrix<double, rowMajor> A(NN, NN);
+    blaze::DynamicVector<double, columnVector> x(NN, 1.0), b(NN, 0.0), r(NN), p(NN), Ap(NN);
+    double alpha, beta, delta;
+
+    // ... Initializing the sparse matrix A
+
+    // Performing the CG algorithm
+    r = b - A * x;
+    p = r;
+    delta = (r, r);
+
+    for (size_t iteration = 0UL; iteration < iterations; ++iteration)
+    {
+        Ap = A * p;
+        alpha = delta / (p, Ap);
+        x += alpha * p;
+        r -= alpha * Ap;
+        beta = (r, r);
+        if (std::sqrt(beta) < 1E-8) break;
+        p = r + (beta / delta) * p;
+        delta = beta;
+    }
+	std::cout << getNumThreads() << '\n';
 	return 0;
 }
